0911659c1b152c5cade83e96a8fad077cb348a83
Nghia Bui <nghia@MacBook-Air.local>
2023-09-13 09:37:28 +0700

    done print food to display

---
+++ Front-end/src/page/merchant/listFood.js
@@ -0,0 +1,65 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
+import {getFood} from "../../service/foodsService";
+import {useEffect} from "react";
+import './listFood.css'
+
+
+export default function ListFood(){
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const food = useSelector((state)=>{
+        return state.food.food
+    })
+    useEffect(() =>{
+        dispatch(getFood())
+    }, [])
+
+    const handleDelete = useDispatch()
+    return(
+
+        <>
+            <div className="title-list">
+                <div className="title-small">
+                    <p style={{fontSize:'30px'}}>Những đồ ăn hiện có</p>
+                </div>
+            </div>
+
+            <div style={{display:'flex', width:'100%', height:'330px'}}>
+                {food.map((item, key)=>(
+                    <div className="card-home">
+                        <div className="row-1 col-10">
+                            <div className="col-2" style={{height: '200px'}}>
+
+                                <div className="card" style={{width: '200px'}}>
+                                    <div className='food-img'>
+                                        <img src={item.imgUrl} className="card-img-top" alt="..." />
+                                    </div>
+
+                                    <div className="card-body">
+                                        <h5 className="card-title">{item.name}</h5>
+                                        <div className="bottom-card" style={{display: 'flex', justifyContent: 'space-between'}}>
+                                            <div className="bottom-card-detail">
+                                                <p style={{marginBottom: '1px', fontSize: '12px', color: '#acacac'}}>{item.note}</p>
+                                                <p style={{fontWeight: 700}}>{item.price}</p>
+                                            </div>
+                                            <div style={{marginTop:'30px', display:'flex', gap:'15px'}}>
+                                                <i  className="fa-solid fa-circle-plus" style={{display: 'flex', alignItems: 'center', color: 'red'}} />
+                                                <i className="fa-solid fa-pen-to-square" style={{display: 'flex', alignItems: 'center', color: 'red'}} />
+                                                <i  className="fa-solid fa-trash" style={{display: 'flex', alignItems: 'center', color: 'red'}} />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+
+
+
+        </>
+    )
+}
--- Front-end/src/redux/store.js
+++ Front-end/src/redux/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import foodReducer from "./merchant/merchantSlice"
 
 export const store = configureStore({
     reducer:{
         user: userReducer,
+        food: foodReducer
     }
 })
